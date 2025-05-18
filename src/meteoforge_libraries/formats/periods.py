2a4fe9a168177ddd7fa222a88d5ecf3ff3a7434d
Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>
2025-05-18 13:13:14 +0200

    develop: in between update
    Signed-off-by: Raoul Linnenbank <58594297+rflinnenbank@users.noreply.github.com>

--- src/meteoforge_libraries/formats/periods.py
+++ src/meteoforge_libraries/formats/periods.py
@@ -2,3 +2,9 @@
 
 #  SPDX-FileCopyrightText: 2019-2025 Alliander N.V.
 #  SPDX-License-Identifier: MPL-2.0
+
+
+class ForgePeriod:
+    """A class representing a time period for meteorological data."""
+
+    ...
